0639393112e10ffda5a6b5f4b339a1aac7913da8
seamuss <s.sands@kainos.com>
2015-02-22 16:12:05 +0000

    Programming 2 changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cachematrix.R
+# Tasks to complete the assignment
+# makeCacheMatrix =>  function to extend a matrix by adding inverse 
+#                     properties
+# cacheSolve => function extending the core solve(x,...) function 
+#               to make use of a cached inverse where available
 
-## Write a short comment describing this function
-
+#
+# Create an extended matrix
+# -get() gets the matrix
+# -set(y) updates the matrix content
+# -setinverse(inverse) set the matrix inverse
+# -getinverse() get the inverse matrix
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initialise the inverse to null
+  i <- NULL
+  # function to update the matrix
+  set <- function(y) {
+    x <<- y
+    # ensure the inverse is returned to null
+    # so cached inverse is not used
+    i <<- NULL
+  }
+  # function to return the matrix
+  get <- function() x
+  # function to set the matrix inverse
+  setinverse <- function(inverse) i <<- inverse
+  # function to return the inverse
+  getinverse <- function() i
+  # Return the special extended matrix as a list 
+  list(get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#
+# Entended solve function to enable caching
+# cacheSolve(x,...) where x,... are matrices
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # return the cached matrix for x where possible
+  i <- x$getinverse()
+  # if a cached inverse exists, return it
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  # set data to the matrix value in x
+  data <- x$get()
+  # use the core solve function as the inverse
+  # is not cached yet
+  i <- solve(data, ...)
+  # update the cached matrix inverse in x
+  x$setinverse(i)
+  # return the inverse matrix
+  i
 }
